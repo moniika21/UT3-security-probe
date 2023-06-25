05914bc14a94a7c0cad5af56c1c674648ad77e8f
Moniika <evancutaia21@gmail.com>
2023-06-26 01:13:52 +0200

    v 2.0

--- src/api.js
+++ src/api.js
@@ -1,6 +1,7 @@
 import { LowSync, JSONFileSync } from "lowdb";
 import { join } from "path";
 import express from "express";
+import cors from "cors";
 
 const app = express();
 const port = 3000;
@@ -8,7 +9,12 @@ const port = 3000;
 // Initialisation de la base de données avec le fichier JSON
 const filename = join(process.cwd(), "db.json");
 const db = new LowSync(new JSONFileSync(filename));
-
+db.read();
+if (db.data.probes === undefined) {
+  db.data.probes = db.data.probes || [];
+  db.write();
+}
+app.use(cors());
 app.use(express.json());
 
 // Middleware d'authentification
@@ -22,32 +28,29 @@ function authenticate(req, res, next) {
 
 // Endpoint pour obtenir l'état de la sonde
 //date -u +"%Y-%m-%dT%H:%M:%SZ"
-/*
-{
-    "probe": {
-        "ipAddress": "192.168.0.1",
-        "status": "online",
-        "lastUpdate": "2023-06-25T12:30:00Z" 
-    }
-}
-*/
-app.get("/api/probe/status", authenticate, (req, res) => {
+
+// {
+//     "probes": [{
+//         "ip": "192.168.0.1",
+//         "status": "online",
+//         "timestamp": "2023-06-25T12:30:00Z"
+//     }]
+// }
+
+app.get("/probes", authenticate, (req, res) => {
   db.read();
-  res.json(db.data.probe);
+  const probes = db.data.probes.sort((a, b) => b.timestamp - a.timestamp);
+  res.json(probes);
 });
 
 // Endpoint pour mettre à jour l'état de la sonde
-app.post("/api/probe/update", authenticate, (req, res) => {
-  const updatedProbe = req.body.probe;
+app.post("/probes", authenticate, (req, res) => {
+  const probe = req.body;
+  probe.timestamp = Date.now();
   db.read();
-  db.data.probe = updatedProbe;
+  db.data.probes = db.data.probes.concat(probe);
   db.write();
-  res.send("L'état de la sonde a été mis à jour avec succès.");
-});
-
-app.get('/api/data', (req, res) => {
-  const data = db.getState();
-  res.json(data);
+  res.sendStatus(200);
 });
 
 app.listen(port, () => {
---
+++ src/server.js
@@ -0,0 +1,18 @@
+import express from "express";
+import path from "path";
+
+const app = express();
+const port = 3001;
+const publicPath = "D:\\Documents\\Github\\Université\\UT3-security-probe";
+// const publicPath = "D:\\Documents\\Github\\Université\\UT3-security-probe";
+
+app.use(express.json());
+
+app.get("/", function (req, res) {
+  console.log(path.dirname(""));
+  res.sendFile(path.join(publicPath, "/src/index.html"));
+});
+
+app.listen(port, () => {
+  console.log(`API en cours d'exécution sur le port ${port}`);
+});
